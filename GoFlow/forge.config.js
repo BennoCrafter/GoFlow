cdbb30b893cc4e7192cf024c2c2efd668c2bd806
benno <BennoCrafterDev@googlemail.com>
2023-10-12 20:15:52 +0200

    updatet style

--- GoFlow/forge.config.js
+++ GoFlow/forge.config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      platforms: ['darwin', "linux"],
     },
     {
       name: '@electron-forge/maker-deb',
---
+++ GoFlow/src/js/Widgets/gif.js
@@ -0,0 +1,13 @@
+import { Widget } from "../widget.js";
+
+export class GifWidget extends Widget {
+  constructor(
+    widgetId,
+    data,
+    uniqueWidgetData
+  ) {
+    super(widgetId, data, uniqueWidgetData);
+
+  }
+ 
+}
