ef310cb6c6a66e29cda66491694c3dfc32f97e52
benno <BennoCrafterDev@googlemail.com>
2023-11-05 13:03:24 +0100

    added idk to much stuff

---
+++ GoFlow/src/Electron/Utils/createDirectory.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+function createDirectory(path, dirName){
+    const fullPath = path + "/" + dirName
+    if (
+        !fs.existsSync(fullPath)
+      ) {
+        fs.mkdir(
+            fullPath,
+          (err) => {
+            if (err) {
+              console.error("Error creating directory:", err);
+            } else {
+              console.log("Directory created successfully");
+            }
+          }
+        );
+      } else {
+        console.log("Directory already exists");
+      }
+}
+
+module.exports = { createDirectory } 
---
+++ IMPORTANTTODO.md
@@ -0,0 +1,8 @@
+# IMPORTANT TODOS
+
+- add new page feature
+
+- add feature to set the lang and long of weather widget
+
+- add vanilla toast lib
+
