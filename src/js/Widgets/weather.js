08e4ec8179649031740ea36a42f7574d8cbcd5fa
benno <BennoCrafterDev@googlemail.com>
2023-11-10 21:58:20 +0100

    added better styling to weather popup. added lat long to loc feature.

--- src/js/Widgets/weather.js
+++ src/js/Widgets/weather.js
@@ -1,80 +1,151 @@
 import { Widget } from "../widget.js";
 
+export class WeatherWidget extends Widget {
+  constructor(widgetId, data, uniqueWidgetData) {
+    super(widgetId, data, uniqueWidgetData);
+    this.getWeatherData();
 
-export class WeatherWidget extends Widget{
-    constructor(widgetId, data, uniqueWidgetData){
-        super(widgetId, data, uniqueWidgetData)
-        this.getWeatherData()
+    this.weatherIconsDict = {
+      0: ["clear-night.svg", "clear-day.svg"],
+      1: ["clear-night.svg", "clear-day.svg"],
+      2: ["partly-cloudy-night.svg", "partly-cloudy-day.svg"],
+      3: ["overcast-night.svg", "overcast-day.svg"],
+      45: ["fog-night.svg", "wo_fog-day.svg"],
+      61: ["partly-cloudy-night-drizzle.svg", "partly-cloudy-day-drizzle.svg"],
+      65: ["partly-cloudy-night-rain.svg", "partly-cloudy-day-rain.svg"],
+      80: ["partly-cloudy-night-rain.svg", "partly-cloudy-day-rain.svg"],
+    };
 
-        this.weatherIconsDict = {0: ["clear-night.svg","clear-day.svg"], 1: ["clear-night.svg","clear-day.svg"], 2: ["partly-cloudy-night.svg", "partly-cloudy-day.svg"], 3: ["overcast-night.svg", "overcast-day.svg"], 45: ["fog-night.svg", "wo_fog-day.svg"], 61: ["partly-cloudy-night-drizzle.svg", "partly-cloudy-day-drizzle.svg"], 65: ["partly-cloudy-night-rain.svg", "partly-cloudy-day-rain.svg"]}
-      
-        if(this.uniqueWidgetData.longitude==null && this.uniqueWidgetData.latitude==null){
-          this.showLocationSelectionPopup()
-          this.submitLocationListener()
-        }else{
-          this.widgetPath.querySelector(".location-popup").style.display = "none"
-          this.widgetPath.querySelector(".weatherWindow").style.display = "block"
+    if (
+      this.uniqueWidgetData.longitude == null &&
+      this.uniqueWidgetData.latitude == null
+    ) {
+      this.showLocationSelectionPopup();
+      this.submitLocationListener();
+    } else {
+      this.widgetPath.querySelector(".location-popup").style.display = "none";
+      this.widgetPath.querySelector(".weatherWindow").style.display = "block";
+    }
+  }
+
+  showLocationSelectionPopup() {
+    this.widgetPath.querySelector(".location-popup").style.display = "block";
+    this.widgetPath.querySelector(".weatherWindow").style.display = "none";
+  }
+
+  getWeatherData() {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${this.uniqueWidgetData.latitude}&longitude=${this.uniqueWidgetData.longitude}&hourly=temperature_2m,precipitation_probability,precipitation,weathercode,is_day&daily=weathercode&current_weather=true&timezone=Europe%2FBerlin`;
+
+    fetch(url)
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw new Error(
+            `Failed to fetch data. Status code: ${response.status}`
+          );
         }
-      }
+      })
+      .then((data) => {
+        // Now you can work with the JSON data
+        console.log(data);
+        // current_weather .. weathercode--- is_day
+        // get loc with long and lang
+        this.setWeatherIcon(
+          1,
+          this.weatherIconsDict[data["current_weather"]["weathercode"]][
+            data["current_weather"]["is_day"]
+          ],
+          data["current_weather"]["temperature"],
+          this.uniqueWidgetData.loc
+        );
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+  // day stands for, which day we should set the weather icon. day 1 is current day
+  setWeatherIcon(day, weatherPath, temp) {
+    const dayPath = this.widgetPath
+      .querySelector(".weatherWindow")
+      .querySelector(`#day${day}`);
+    // set img src
+    const imgSrc = dayPath.querySelector("#weather-src");
+    imgSrc.src = `resources/weather-icons/${weatherPath}`;
+    // set temperature
+    dayPath.querySelector(".temperature").textContent = temp;
 
-    showLocationSelectionPopup(){
-      this.widgetPath.querySelector(".location-popup").style.display = "block"
-      this.widgetPath.querySelector(".weatherWindow").style.display = "none"
-    }
+    // set location
+    dayPath.querySelector(".location").textContent = this.uniqueWidgetData.loc;
+  }
 
-    getWeatherData(){
-        const url = `https://api.open-meteo.com/v1/forecast?latitude=${this.uniqueWidgetData.latitude}&longitude=${this.uniqueWidgetData.longitude}&hourly=temperature_2m,precipitation_probability,precipitation,weathercode,is_day&daily=weathercode&current_weather=true&timezone=Europe%2FBerlin`;
+  submitLocationListener() {
+    this.widgetPath
+      .querySelector(".location-popup")
+      .querySelector("#submitWeatherLoc")
+      .addEventListener("click", (event) => {
+        if (
+          this.widgetPath
+            .querySelector(".location-popup")
+            .querySelector("#latitudeField").value !== "" &&
+          this.widgetPath
+            .querySelector(".location-popup")
+            .querySelector("#longitudeField").value !== ""
+        ) {
+          this.uniqueWidgetData.latitude = this.widgetPath
+            .querySelector(".location-popup")
+            .querySelector("#latitudeField").value;
+          this.uniqueWidgetData.longitude = this.widgetPath
+            .querySelector(".location-popup")
+            .querySelector("#longitudeField").value;
 
-        fetch(url)
-          .then(response => {
-            if (response.status === 200) {
-              return response.json();
-            } else {
-              throw new Error(`Failed to fetch data. Status code: ${response.status}`);
-            }
-          })
-          .then(data => {
-            // Now you can work with the JSON data
-            console.log(data);
-            // current_weather .. weathercode--- is_day
-            // get loc with long and lang
-            const loc = "Berlin"
-            this.setWeatherIcon(1, this.weatherIconsDict[data["current_weather"]["weathercode"]][data["current_weather"]["is_day"]], data["current_weather"]["temperature"], loc)
-          })
-          .catch(error => {
-            console.error(error);
-          });
-          
-    }
-    // day stands for, which day we should set the weather icon. day 1 is current day
-    setWeatherIcon(day, weatherPath, temp, loc){
-        const dayPath = this.widgetPath.querySelector(".weatherWindow").querySelector(`#day${day}`)
-        // set img src
-        const imgSrc = dayPath.querySelector("#weather-src");
-        imgSrc.src = `resources/weather-icons/${weatherPath}`;
-        // set temperature
-        dayPath.querySelector(".temperature").textContent = temp;
+          this.widgetPath
+            .querySelector(".location-popup")
+            .querySelector("#latitudeField").value = "";
+          this.widgetPath
+            .querySelector(".location-popup")
+            .querySelector("#longitudeField").value = "";
 
-        // set location
-        dayPath.querySelector(".location").textContent = loc
+          this.widgetPath.querySelector(".location-popup").style.display =
+            "none";
+          this.widgetPath.querySelector(".weatherWindow").style.display =
+            "block";
 
-    }
+          const url = `https://geocode.maps.co/reverse?lat=${this.uniqueWidgetData.latitude}&lon=${this.uniqueWidgetData.longitude}`;
+          fetch(url)
+            .then((response) => {
+              if (response.status === 200) {
+                return response.json();
+              } else {
+                throw new Error(
+                  `Failed to fetch data. Status code: ${response.status}`
+                );
+              }
+            })
+            .then((data) => {
+              // Now you can work with the JSON data
+              if (data.address.hasOwnProperty("village")) {
+                this.uniqueWidgetData.loc = data.address.village;
+              } else {
+                this.uniqueWidgetData.loc = data.address.city;
+              }
+              this.getWeatherData()
+              this.setWeatherIcon(
+                1,
+                this.weatherIconsDict[data["current_weather"]["weathercode"]][
+                  data["current_weather"]["is_day"]
+                ],
+                data["current_weather"]["temperature"]);
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        }
+      });
+  }
 
-    submitLocationListener(){
-    this.widgetPath.querySelector(".location-popup").querySelector("#submitWeatherLoc").addEventListener("click", (event) => {
-      this.uniqueWidgetData.latitude = this.widgetPath.querySelector(".location-popup").querySelector("#latitudeField").value
-      this.uniqueWidgetData.longitude = this.widgetPath.querySelector(".location-popup").querySelector("#longitudeField").value
-      
-      this.widgetPath.querySelector(".location-popup").querySelector("#latitudeField").value = ""
-      this.widgetPath.querySelector(".location-popup").querySelector("#longitudeField").value = ""
-    
-      this.widgetPath.querySelector(".location-popup").style.display = "none"
-      this.widgetPath.querySelector(".weatherWindow").style.display = "block"
-    })
-    }
-
-    enterEditMode(){
-      this.showLocationSelectionPopup()
-      this.submitLocationListener()
-    }
-} 
+  enterEditMode() {
+    this.showLocationSelectionPopup();
+    this.submitLocationListener();
+  }
+}
